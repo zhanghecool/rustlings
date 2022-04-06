abf181eda9c3c96725a2f4a88d7e06d1ce7bc837
ragreenburg <ragreenburg@gmail.com>
2022-04-06 00:29:27 -0700

    fix(move_semantics2): Add comment

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -1,6 +1,6 @@
 // move_semantics2.rs
 // Make me compile without changing line 13 or moving line 10!
-// Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
+// Execute `rustlings hint move_semantics2` for hints :)
 
 fn main() {
     let mut vec0 = Vec::new();
